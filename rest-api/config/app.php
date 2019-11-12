216ea7e74887b1c5d397a3fb15c97f459a2b64ed
vasilijus <vasiliokas@gmail.com>
2019-11-12 23:28:33 +0000

    added CORS setting to project rest-api

--- rest-api/config/app.php
+++ rest-api/config/app.php
@@ -161,6 +161,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        // Adding CORS
+        Barryvdh\Cors\ServiceProvider::class,
+
 
         /*
          * Package Service Providers...
