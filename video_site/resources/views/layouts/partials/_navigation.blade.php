81b2b8b7fbbaf829facb131088f1bd86f88b3bcd
Sergej <sergej@computerassistance.co.uk>
2019-11-15 17:45:33 +0000

    Created Service Provider, and added ViewComposers to HTTP

--- video_site/resources/views/layouts/partials/_navigation.blade.php
+++ video_site/resources/views/layouts/partials/_navigation.blade.php
@@ -34,6 +34,11 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                            <a class="dropdown-item" href="{{ url('/channel/' . $channel->slug) }}">Your channel</a>
+                            <a class="dropdown-item" href="{{ url('/channel/' . $channel->slug) . '/edit' }}"> Channel settings</a>
+
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
