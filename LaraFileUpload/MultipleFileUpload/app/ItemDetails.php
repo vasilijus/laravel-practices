06d739d61fd9f232c5e50c65998fe2aea9e4f78e
Sergej <sergej@computerassistance.co.uk>
2020-01-16 18:32:45 +0000

    multiple files upload

---
+++ LaraFileUpload/MultipleFileUpload/app/ItemDetails.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemDetails extends Model
+{
+    //
+    protected $fillable = ['item_id','filename'];
+
+    public function item()
+    {
+    
+        return $this->belongsTo('App\Item');
+    
+    }
+    
+}
